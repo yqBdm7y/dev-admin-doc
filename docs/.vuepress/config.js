c132db8f3e79dbc4df5f09498c9722bd11078fa4
Your Name <you@example.com>
2024-06-09 22:05:55 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  base: '/dev-admin-doc/',
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
